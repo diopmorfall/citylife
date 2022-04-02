716461482b661533b49d15c36486850f101fe2d7
diopmorfall <morfall.papydiop99@gmail.com>
2022-04-02 22:29:58 +0200

    Integration of axios for the api call

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,24 +1,16 @@
+import axios from "axios";
 import { showData, showErrorMessage } from "./animations";
 import { City } from "./city";
 
 export async function fetchUrl(city){
-    try{
-        //todo: do it with axios and lodash
-        let response = await fetch(
-            `https://api.teleport.org/api/urban_areas/slug:${city.replace(/ /g, "-").toLowerCase()}/scores/`
-        );
+    axios.get(
+        `https://api.teleport.org/api/urban_areas/slug:${city.replace(/ /g, "-").toLowerCase()}/scores/`
+    )
+    .then(response => showData(new City(response.data)))
 
-        if(!response.ok) throw response.status;
-
-        // let cityDetails = await response.json();
-        // console.log(cityDetails);
-        let cityObj = new City(await response.json());
-        // console.log(cityObj);
-        showData(cityObj);
-    } catch(status){
-        showErrorMessage(
-            status == 404 ? "City not found, make sure you typed it right" : 
+    .catch(err => showErrorMessage(err.response.status == 404 ?
+            "City not found, make sure you typed it right" : 
             "Something went wrong, please try later"
-        );
-    }
+        )
+    );
 }
