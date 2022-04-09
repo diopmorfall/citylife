e06538167d1a4e11f61594c774a580df8ef373e6
diopmorfall <morfall.papydiop99@gmail.com>
2022-04-09 22:04:57 +0200

    Refactoring of the api call within try-catch

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,15 +4,16 @@ import { introSection } from "./dom-elements";
 import { City } from "./city";
 
 export async function fetchUrl(city){
-    axios.get(
-        `https://api.teleport.org/api/urban_areas/slug:${city.replace(/ /g, "-").toLowerCase()}/scores/`
-    )
-    .then(response => showData(new City(response.data)))
-
-    .catch(err => showErrorMessage(err.response.status == 404 ?
+    try {
+        let response = await axios.get(
+            `https://api.teleport.org/api/urban_areas/slug:${city.replace(/ /g, "-").toLowerCase()}/scores/`
+        );
+        showData(new City(response.data));
+    } catch(error) {
+        showErrorMessage(error.response.status == 404 ?
             "City not found, make sure you typed it right" : 
             "Something went wrong, please try later",
             introSection
-        )
-    );
+        );
+    }
 }
